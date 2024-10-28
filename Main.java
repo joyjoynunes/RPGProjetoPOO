f9fa3b23165f124a2dfcb8d51510a71f0ebd0f15
Joyce Nunes <joycenunes1721@gmail.com>
2024-10-27 22:08:31 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        //System.out.printf("Hello and welcome!");
+package inicio;
+import com.sun.tools.javac.Main;
+import personagem.Arthor;
+import java.io.InputStream;
+import java.util.Scanner;
 
-        //for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            //System.out.println("i = " + i);
-        }
+public class main {
+
+    public Scanner scan = new Scanner(System.in);
+    public String nome;
+
+    public static void main (String[] args) {
+
+        //Main programa = new Main();
+        //programa.inicio();
+
+            System.out.println("Bem vindo ao Fantástico reino de Eldoria\n" +
+                    "Antes de começarmos o Setup inicial, por favor me informe como deseja ser chamado:");
+           // String nome = scan.nextLine();
+
+            System.out.println("Legal ^^!\n" +
+                    "Seja bem vindo(a)");
+
+
+       // for (int i = 1; i <= 8; i++)
+        //{
+          //  System.out.println("i = " + i);
+        //}
     }
+
+}
+
